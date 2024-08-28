d2caaddc92963b165810146370820155479d2de5
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-08-28 12:51:49 -0500

    Se crea página Dashboard con sus rutas y componentes

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,23 @@
+import SideBar from './components/SideBar'
+import Alojamientos from '../Alojamientos/Alojamientos'
+import Actividades from '../Actividades/Actividades'
+import Servicios from '../Servicios/Servicios'
+import NotFound from '../NotFound/NotFound'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+function Dashboard() {
+	return (
+		<main className='bg-white w-full h-dvh flex flex-row gap-10'>
+			<SideBar />
+			<Routes>
+				<Route path='/' element={<Navigate to='alojamientos' />} />
+				<Route path='alojamientos' element={<Alojamientos />} />
+				<Route path='actividades' element={<Actividades />} />
+				<Route path='servicios' element={<Servicios />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</main>
+	)
+}
+
+export default Dashboard
