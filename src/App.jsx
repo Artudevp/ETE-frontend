085bb830387ad954cf1ddba07b4fddf051f10e41
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-09-28 16:00:19 -0500

    Se elimina librería UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import AppRoutes from './routes/AppRoutes'
 import './App.css'
 
-import 'primereact/resources/themes/lara-light-indigo/theme.css' //theme
-import 'primereact/resources/primereact.min.css' //core css
-import 'primeicons/primeicons.css'
-
 function App() {
 	return (
 		<>
