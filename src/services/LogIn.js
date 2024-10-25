99b2a5ffaa6d205000bdcbb5604abf6c8b69c36e
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-25 15:23:08 -0500

    Se crea servicio de Login con la decodificación del token para obtener información del usuario

---
+++ src/services/LogIn.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { jwtDecode } from 'jwt-decode'
+
+const loginService = async (username, contraseña) => {
+	console.log({ username, contraseña })
+	try {
+		const response = await axios.post('http://localhost:8080/auth/login', {
+			username,
+			contraseña,
+		})
+		const token = response.data.token
+		const decoded = jwtDecode(token)
+		const role = decoded.role
+		localStorage.setItem('token', token)
+		localStorage.setItem('role', role)
+		return role
+	} catch (error) {
+		console.error('Error en el login:', error.response?.data || error.message)
+		throw error
+	}
+}
+
+export default loginService
