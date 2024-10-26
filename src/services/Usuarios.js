1dbeb920909965eeaa93f23c5a9174c3de7ab739
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-10-26 12:44:30 -0500

    Se actualiza url de usuarios

--- src/services/Usuarios.js
+++ src/services/Usuarios.js
@@ -13,7 +13,7 @@ const getUsers = async () => {
 
 const addUser = async user => {
 	try {
-		const response = await axiosInstance.post('/usuarios', user)
+		const response = await axiosInstance.post('/users', user)
 		return response.data
 	} catch (error) {
 		throw new Error(
@@ -24,10 +24,7 @@ const addUser = async user => {
 
 const updateUser = async user => {
 	try {
-		const response = await axiosInstance.put(
-			`/usuarios/${user.id_usuario}`,
-			user,
-		)
+		const response = await axiosInstance.put(`/users/${user.id_usuario}`, user)
 		return response.data
 	} catch (error) {
 		throw new Error(
@@ -38,7 +35,7 @@ const updateUser = async user => {
 
 const deleteUser = async id => {
 	try {
-		await axiosInstance.delete(`/usuarios/${id}`)
+		await axiosInstance.delete(`/users/${id}`)
 	} catch (error) {
 		throw new Error(
 			`HTTP error! status: ${error.request.status}, message: ${error.message}`,
