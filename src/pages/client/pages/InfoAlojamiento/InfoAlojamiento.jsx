7dae3ddf2a84483466333b212525fdba6689b203
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-08 16:21:12 -0500

    Se recibe desde el contexto el hospedaje seleccionado

--- src/pages/client/pages/InfoAlojamiento/InfoAlojamiento.jsx
+++ src/pages/client/pages/InfoAlojamiento/InfoAlojamiento.jsx
@@ -1,62 +1,43 @@
 import habitación from '../../../../assets/habitacion.png'
-import { useState } from 'react'
-import { FaRegUserCircle } from 'react-icons/fa'
 import Style from './InfoAlojamiento.module.css'
+import { useClient } from '../../../../context/ClientProviders'
 
 function InfoAlojamiento() {
-	const [date, setDate] = useState(null)
-	const [valueStar, setValueStar] = useState(4)
+	const { hospedajeSelected } = useClient()
+	const {
+		tipo_hab,
+		precio_hab,
+		disponibilidad,
+		capacidad,
+		descripcion_hab,
+		disponible,
+	} = hospedajeSelected
+
 	return (
 		<div className={Style.main}>
-			<h1>Sobre la habitación</h1>
-			<div className={Style.container}>
-				<div className={Style.boxLeft}>
-					<img src={habitación} alt='Image habitación' />
-					<div className={Style.descripHabitacion}>
-						<h2>Habitación 2 personas</h2>
-						<p>
-							Cuenta con un espacio amplio para que pases tu estadía lo más
-							cómodo posible, cuenta con una sala de estar con sus muebles para
-							pasar la tarde en familia.
-						</p>
-						<p>
-							La habitación cuenta con una cama matrimonial, dos camas
-							individuales.
-						</p>
-						<p className={Style.precioHab}>Precio: $30</p>
-					</div>
-					<div className={Style.calificacionHab}>
-						<h2>Calificaciones</h2>
-						<div className={Style.calificacionList}>
-							<div className={Style.calificacionBox}>
-								<div className={Style.userName}>
-									<FaRegUserCircle />
-									<h3>Usuario</h3>
-								</div>
-								<p>
-									Estadía super cómoda, cuenta con un amplio espacio para estar
-									comodo!
-								</p>
-							</div>
-							<div className={Style.calificacionBox}>
-								<div className={Style.userName}>
-									<FaRegUserCircle />
-									<h3>Usuario</h3>
-								</div>
-								<p>
-									Estadía super cómoda, cuenta con un amplio espacio para estar
-									comodo!
-								</p>
+			{disponible ? (
+				<>
+					<h1>Sobre la habitación</h1>
+					<div className={Style.container}>
+						<div className={Style.boxLeft}>
+							<img src={habitación} alt='Image habitación' />
+							<div className={Style.descripHabitacion}>
+								<h2>{tipo_hab}</h2>
+								<p>{descripcion_hab}</p>
+								<p className={Style.precioHab}>Precio: ${precio_hab}</p>
+								<p>Capacidad: {capacidad}</p>
+								<p>Habitaciones disponibles: {disponibilidad}</p>
 							</div>
 						</div>
+						<div className={Style.boxRight}>
+							<h2>No esperes más, reserva ahora tu habitación {tipo_hab}</h2>
+							<button>Reservar ahora</button>
+						</div>
 					</div>
-				</div>
-				<div className={Style.boxRight}>
-					<h2>Elige tu fecha de estadía</h2>
-
-					<button>Reservar ahora</button>
-				</div>
-			</div>
+				</>
+			) : (
+				<h1>Habitación no disponible</h1>
+			)}
 		</div>
 	)
 }
