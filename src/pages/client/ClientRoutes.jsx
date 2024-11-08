3ad11da605ad300738d73a2cfc699e708e3f4b48
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-08 16:26:41 -0500

    Se importa el Client Provider y se añade ruta para la actividad seleccionada

--- src/pages/client/ClientRoutes.jsx
+++ src/pages/client/ClientRoutes.jsx
@@ -2,15 +2,18 @@ import Style from './ClientRoutes.module.css'
 import SideBar from './components/SideBar/SideBar'
 import Alojamientos from './pages/Alojamientos/Alojamientos'
 import InfoAlojamiento from './pages/InfoAlojamiento/InfoAlojamiento'
+import InfoActividad from './pages/InfoActividad/InfoActividad'
 import Actividades from './pages/Actividades/Actividades'
 import Servicios from './pages/Servicios/Servicios'
 import NotFound from './pages/NotFound/NotFound'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { ClientProvider } from '../../context/ClientProvider'
 
 const routes = [
 	{ path: 'alojamientos', element: <Alojamientos /> },
 	{ path: 'alojamientos/habitacion', element: <InfoAlojamiento /> },
 	{ path: 'actividades', element: <Actividades /> },
+	{ path: 'actividades/actividad', element: <InfoActividad /> },
 	{ path: 'servicios', element: <Servicios /> },
 	{ path: '*', element: <NotFound /> },
 ]
@@ -20,12 +23,18 @@ function ClientRoutes() {
 		<main className={Style.main}>
 			<SideBar />
 			<div className={Style.content}>
-				<Routes>
-					<Route path='/' element={<Navigate to='alojamientos' />} />
-					{routes.map(route => (
-						<Route key={route.path} path={route.path} element={route.element} />
-					))}
-				</Routes>
+				<ClientProvider>
+					<Routes>
+						<Route path='/' element={<Navigate to='alojamientos' />} />
+						{routes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={route.element}
+							/>
+						))}
+					</Routes>
+				</ClientProvider>
 			</div>
 		</main>
 	)
