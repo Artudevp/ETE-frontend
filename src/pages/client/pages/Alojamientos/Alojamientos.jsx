4bef12fff1d443150b2d3ef08ea0abf6633f8e85
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-11-08 16:25:38 -0500

    Se recibe desde el contexto los datos a mostrar

--- src/pages/client/pages/Alojamientos/Alojamientos.jsx
+++ src/pages/client/pages/Alojamientos/Alojamientos.jsx
@@ -1,23 +1,45 @@
-import Card from '../../components/Card/Card'
 import Style from './Alojamientos.module.css'
+import Card from '../../components/Card/Card'
+import { useAlojamientos, useClient } from '../../../../context/ClientProviders'
 
 function Alojamientos() {
+	const { alojamientos } = useAlojamientos()
+	const { setHospedajeSelected } = useClient()
+
+	const handleHospedajeSelected = hospedaje => {
+		setHospedajeSelected(hospedaje)
+	}
+
 	return (
 		<div className={Style.containerAlojamientos}>
-			<div className={Style.containerAlojamiento}>
+			{/* <div className={Style.containerAlojamiento}>
 				<h1>Alojamientos reservados</h1>
 				<div className={Style.alojamientoList}>
 					<Card />
 					<Card />
 					<Card />
 				</div>
-			</div>
+			</div> */}
 			<div className={Style.containerAlojamiento}>
 				<h1>Alojamientos disponibles</h1>
 				<div className={Style.alojamientoList}>
-					<Card />
-					<Card />
-					<Card />
+					{alojamientos?.length > 0 ? (
+						alojamientos.map(
+							(item, index) =>
+								item.disponible && (
+									<Card
+										key={index}
+										tipo_prod={item.tipo_hab}
+										precio_prod={item.precio_hab}
+										producto={item}
+										handleHospedajeSelected={handleHospedajeSelected}
+										ruta='alojamientos/habitacion'
+									/>
+								),
+						)
+					) : (
+						<p>No hay alojamientos disponibles</p>
+					)}
 				</div>
 			</div>
 		</div>
